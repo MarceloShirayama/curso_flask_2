09efd4e2603c9f6a4eeb659e2dcd68b1f652095f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2020-08-01 14:15:15 -0300

    create template and blueprint

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+from delivery.ext import site
 
 
 def create_app():
     """Factory principal"""
     app = Flask(__name__)
+    site.init_app(app)
     return app
---
+++ delivery/delivery/ext/site/main.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, render_template
+
+
+bp = Blueprint("site", __name__)
+
+@bp.route("/")
+def index():
+    return render_template(
+        "index.html",
+        name="Marcelo Shirayama",
+        company="CodeShow",
+        texto1="Foods Delivery",
+        texto2="Vários pratos a sua escolha:",
+        comidas=["pizza", "lanche", "japonês"]
+    )
