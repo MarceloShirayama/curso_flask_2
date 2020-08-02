42450118163e4b41addeb034acab9340301b1e09
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2020-08-02 19:33:34 -0300

    Banco de dados com SQLAlchemy

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-from delivery.ext import site, toolbar, config
+from delivery.ext import site, toolbar, config, db, cli
 
 
 def create_app():
     """Factory principal"""
     app = Flask(__name__)
     config.init_app(app)
+    db.init_app(app)
+    cli.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
     return app
--- delivery/delivery/ext/cli/__init__.py
+++ delivery/delivery/ext/cli/__init__.py
@@ -1,1 +1,41 @@
- 
+import click
+from delivery.ext.db import db
+from delivery.ext.site import models
+
+
+def init_app(app):
+    
+    @app.cli.command()
+    def create_db():
+        """Este comando cria o db"""
+        db.create_all()
+
+
+    @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--passwd", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, passwd, admin):
+        """Este comando adiciona um novo usuário"""
+        user = models.User(
+            email=email,
+            passwd=passwd,
+            admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        click.echo(f"Usuário {email} criado com sucesso!")
+
+
+    @app.cli.command()        
+    def listar_pedidos():
+        """Este comando lista os pedidos"""
+        click.echo("lista de pedidos")
+
+
+    @app.cli.command()
+    def listar_usuarios():
+        """Este comando lista os usuários"""
+        click.echo("lista de usuários")
+    
