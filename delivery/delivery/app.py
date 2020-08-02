ced2594f2de898cf69c1106aa5cc6c387f923774
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2020-08-02 05:19:06 -0300

    Add templates and toolbar

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-from delivery.ext import site
+from delivery.ext import site, toolbar, config
 
 
 def create_app():
     """Factory principal"""
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     return app
--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -1,15 +1,36 @@
 from flask import Blueprint, render_template
 
 
+nameby="Marcelo Shirayama"
+company="CodeFoods"
+
 bp = Blueprint("site", __name__)
+
 
 @bp.route("/")
 def index():
     return render_template(
         "index.html",
-        name="Marcelo Shirayama",
-        company="CodeShow",
-        texto1="Foods Delivery",
-        texto2="Vários pratos a sua escolha:",
-        comidas=["pizza", "lanche", "japonês"]
+        name=nameby,
+        site_name=company,
+        site_subtitle="O melhor site de delivery",
+    )
+
+
+@bp.route("/sobre")
+def about():
+    return render_template(
+        "about.html",
+        name=nameby,
+        site_name=company,
+        site_subtitle="O melhor site de delivery de comida vegan do mundo! da twitch! e do universo!!!",
+    )
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template(
+        "restaurants.html",
+        name=nameby,
+        site_name=company
     )
